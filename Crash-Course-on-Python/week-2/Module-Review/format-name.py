d8db1d77b5c376197c88a58d8c18cf7eea0a8c12
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-02-25 20:59:01 +0700

    initial commit

---
+++ Crash-Course-on-Python/week-2/Module-Review/format-name.py
@@ -0,0 +1,44 @@
+# Complete the body of the format_name function. This function receives the first_name and last_name parameters and
+# then returns a properly formatted string.
+
+# Specifically:
+
+# If both the last_name and the first_name parameters are supplied, the function should return like so:
+# print(format_name("Ella", "Fitzgerald"))
+# Name: Fitzgerald, Ella
+
+# If only one name parameter is supplied (either the first name or the last name) , the function should return like so:
+# print(format_name("Adele", ""))
+# Name: Adele
+
+# or
+# print(format_name("", "Einstein"))
+# Name: Einstein
+
+# Finally, if both names are blank, the function should return the empty string:
+# print(format_name("", ""))
+
+# Implement below:
+def format_name(first_name, last_name):
+    # code goes here
+    if first_name != "" and last_name != "":
+        string = "Name: " + last_name + ", " + first_name
+    elif first_name != "" or last_name != "":
+        string = "Name: " + first_name + last_name
+    else:
+        string = ""
+
+    return string
+
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
